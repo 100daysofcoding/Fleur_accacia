4643098539c726c29c8e09d9694df843a1fb9c23
Gracia GOKAR <graciajulienne@gmail.com>
2024-02-27 22:26:09 +0000

    new btn

--- day50/chrono.py
+++ day50/chrono.py
@@ -6,30 +6,31 @@ from tkinter import messagebox
 
 fenetre = Tk()
 fenetre.title("Chronomètre")
-fenetre['bg']='#CCEDFF'
+fenetre['bg'] = '#CCEDFF'
 fenetre.geometry("300x250")
 
-
-
-hour=StringVar()
-minute=StringVar()
-second=StringVar()
-
+hour = StringVar()
+minute = StringVar()
+second = StringVar()
 
 hour.set("00")
 minute.set("00")
 second.set("00")
 
+hourEntry = Entry(fenetre, width=3, font=("Arial", 18, ""), textvariable=hour)
+hourEntry.place(x=80, y=20)
 
-hourEntry= Entry(fenetre, width=3, font=("Arial",18,""),textvariable=hour)
-hourEntry.place(x=80,y=20)
+minuteEntry = Entry(fenetre, width=3, font=("Arial", 18, ""), textvariable=minute)
+minuteEntry.place(x=130, y=20)
 
-minuteEntry= Entry(fenetre, width=3, font=("Arial",18,""),textvariable=minute)
-minuteEntry.place(x=130,y=20)
+secondEntry = Entry(fenetre, width=3, font=("Arial", 18, ""), textvariable=second)
+secondEntry.place(x=180, y=20)
 
-secondEntry= Entry(fenetre, width=3, font=("Arial",18,""),textvariable=second)
-secondEntry.place(x=180,y=20)
+paused = False  # État de pause initial
 
+def pause():
+    global paused
+    paused = not paused
 
 def submit():
     try:
@@ -43,24 +44,30 @@ def submit():
         messagebox.showerror("Erreur", str(e))
     else:
         while temp > -1:
-            mins, secs = divmod(temp, 60)
-            hours = 0
-            if mins > 60:
-                hours, mins = divmod(mins, 60)
-            hour.set("{0:02d}".format(hours))
-            minute.set("{0:02d}".format(mins))
-            second.set("{0:02d}".format(secs))
-            fenetre.update()
-            time.sleep(1)
-            if temp == 0:
-                messagebox.showinfo("Compte à rebours terminé", "Le temps est écoulé")
-            temp -= 1
+            if paused:
+                # Si en pause, attendre 1 seconde sans décrémenter le temps
+                time.sleep(1)
+            else:
+                # Sinon, continuer à décrémenter le temps et mettre à jour l'interface utilisateur
+                mins, secs = divmod(temp, 60)
+                hours = 0
+                if mins > 60:
+                    hours, mins = divmod(mins, 60)
+                hour.set("{0:02d}".format(hours))
+                minute.set("{0:02d}".format(mins))
+                second.set("{0:02d}".format(secs))
+                fenetre.update()
+                time.sleep(1)
+                if temp == 0:
+                    messagebox.showinfo("Compte à rebours terminé", "Le temps est écoulé")
+                temp -= 1
 
+begin = PhotoImage(file='day50/jouer.png')
+begin_btn = Button(fenetre, image=begin, command=submit, cursor="hand2", bg='#CCEDFF')
+begin_btn.place(x=25, y=120)
 
-begin= PhotoImage(file='day50/jouer.png')
-btn=Button(fenetre,image=begin,command= submit,cursor="hand2", bd='5',bg='#CCEDFF')
-btn.place(x = 105,y = 120)
-
-
+pause_img = PhotoImage(file='day50/bouton-pause.png')
+pause_btn = Button(fenetre, image=pause_img, command=pause, cursor="hand2", bg='#CCEDFF')
+pause_btn.place(x=125, y=120)
 
 fenetre.mainloop()
