19581af2ebcb05ac093dd5284b0a2d1d006a4e74
Gracia GOKAR <graciajulienne@gmail.com>
2024-04-16 23:15:13 +0000

    dernier repos....

--- day100/end.py
+++ day100/end.py
@@ -1,14 +1,27 @@
 import tkinter as tk
+from PIL import Image, ImageTk
+
+def afficher_gif():
+    img = Image.open("day100/voir.jpg")
+    img = ImageTk.PhotoImage(img)
+    label.configure(image=img)
+    label.image = img
+
 
 fenetre = tk.Tk()
-fenetre.config(border=10,background="#A2DBDF")
 fenetre.title= ("Message de fin")
 
-texte = """ Bonsoir cher tous """
-    
+texte = """ Ladies and gentleman ......"""
+
+bouton = tk.Button(fenetre, text="Appuyer pour voir le rendu", command=afficher_gif)
+bouton.pack(pady=10)
+
+
+label = tk.Label(fenetre)
+label.pack()
 
 widget_texte = tk.Text(fenetre)
 widget_texte.insert(tk.END, texte)
 widget_texte.pack()
-
+ 
 fenetre.mainloop()
